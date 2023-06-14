251eadca7b911bcfdd8fa4bbc79a248b7de02086
hgyeom <rrune3@naver.com>
2023-06-14 17:19:17 +0900

    form css 수정

--- solo/002.ToDo/todolist/src/App.jsx
+++ solo/002.ToDo/todolist/src/App.jsx
@@ -1,5 +1,6 @@
 import Todo from "components/Todo";
 import Form from "components/Form";
+import Header from "components/Header";
 import { useState } from "react";
 import "App.css";
 
@@ -7,12 +8,7 @@ function App() {
   const [todoList, setTodoList] = useState([
     { id: 1, title: "리액트", body: "리액트 과제 끝내기", isDone: true },
     { id: 2, title: "리액트", body: "리액트 강의 다시보기", isDone: false },
-    {
-      id: 3,
-      title: "오버워치",
-      body: "50판 채워서 랭커딱지 달기",
-      isDone: false,
-    },
+    { id: 3, title: "알고리즘", body: "프로그래머스 문제 풀기", isDone: false },
   ]);
 
   const onRemoveHandler = (id) => {
@@ -34,9 +30,7 @@ function App() {
 
   return (
     <div className="layout">
-      <header className="app-header">
-        <h1>My Todo List</h1>
-      </header>
+      <Header />
       <Form todoList={todoList} setTodoList={setTodoList} />
       <Todo
         todoList={todoList}
