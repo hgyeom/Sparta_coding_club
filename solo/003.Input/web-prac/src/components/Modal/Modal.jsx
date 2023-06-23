951ec4d880e245740894c292e86f9ba56ff42b1c
hgyeom <rrune3@naver.com>
2023-06-23 13:22:58 +0900

    update modal

--- solo/003.Input/web-prac/src/components/Modal/Modal.jsx
+++ solo/003.Input/web-prac/src/components/Modal/Modal.jsx
@@ -3,19 +3,31 @@ import ModalCotroll from "./ModalCotroll";
 
 function Modal() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [number, setNumber] = useState("");
 
   // 모달창 노출
-  const showModal = () => {
-    setModalOpen(true);
+  const showModal = (e) => {
+    // console.log(e.target.value);
+    setNumber(e.target.value);
+    setModalOpen(true, e.target);
   };
 
   return (
     <div>
       <h1>Modal</h1>
-      <button onClick={showModal}>open modal</button>
-      {modalOpen && <ModalCotroll setModalOpen={setModalOpen} number="1" />}
+      <button onClick={showModal} value="one">
+        open modal
+      </button>
+      {modalOpen && (
+        <ModalCotroll setModalOpen={setModalOpen} number={number} />
+      )}
 
-      <button>open modal</button>
+      <button onClick={showModal} value="two">
+        open modal
+      </button>
+      {modalOpen && (
+        <ModalCotroll setModalOpen={setModalOpen} number={number} />
+      )}
     </div>
   );
 }
--- solo/003.Input/web-prac/src/components/Modal/ModalCotroll.jsx
+++ solo/003.Input/web-prac/src/components/Modal/ModalCotroll.jsx
@@ -1,5 +1,5 @@
-import { styled } from "styled-components";
-
+import { styled, css } from "styled-components";
+import { useEffect, useRef } from "react";
 const ModalContainer = styled.div`
   /* 모달창 크기 */
   width: 300px;
@@ -22,16 +22,47 @@ const ModalContainer = styled.div`
   border-radius: 8px;
 `;
 
-const ModalCloseButton = styled.button``;
+const ModalCloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
 function ModalCotroll({ setModalOpen, number }) {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalOpen(false);
+      }
+    };
+
+    if (number === "two") {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [number, setModalOpen]);
+
   const closeModal = () => {
     setModalOpen(false);
   };
-  return (
-    <ModalContainer>
-      <p>바깥을 눌러도 종료되지 않는 모달.</p>
-      <ModalCloseButton onClick={closeModal}>취소</ModalCloseButton>
-      <ModalCloseButton>확인</ModalCloseButton>
+
+  return number === "one" ? (
+    <ModalContainer ref={modalRef}>
+      <p>
+        닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.
+      </p>
+      <button onClick={closeModal}>취소</button>
+      <button>확인</button>
+    </ModalContainer>
+  ) : (
+    <ModalContainer ref={modalRef}>
+      <ModalCloseButton onClick={closeModal}>X</ModalCloseButton>
+      <p>닫기 버튼 1개가 있고, 외부 영역을 누르면 모달이 닫혀요.</p>
     </ModalContainer>
   );
 }
